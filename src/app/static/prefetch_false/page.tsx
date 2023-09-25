af58bb5647cf1caf9cb1d02dddb969170d521e55
Tomoya Kashifuku <tnyo43@gmail.com>
2023-09-25 13:23:53 +0900

    add pages

---
+++ src/app/static/prefetch_false/page.tsx
@@ -0,0 +1,38 @@
+import Link from "next/link";
+import React from "react";
+
+export const dynamic = "force-static";
+
+export default function AppStaticNoPrefetch() {
+  return (
+    <div>
+      <Link href="/">root</Link>
+      <ul>
+        <li>
+          <Link href="/static">/static</Link>
+        </li>
+        <li>
+          <Link href="/static/prefetch_false" prefetch={false}>
+            /static (no prefetch)
+          </Link>
+        </li>
+        <li>
+          <Link href="/dynamic">/dynamic</Link>
+        </li>
+        <li>
+          <Link href="/dynamic/prefetch_false" prefetch={false}>
+            /dynamic (no prefetch)
+          </Link>
+        </li>
+        <li>
+          <Link href="https://nextjs.org/">external link</Link>
+        </li>
+        <li>
+          <Link href="https://nextjs.org/docs" prefetch={false}>
+            external link (no prefetch)
+          </Link>
+        </li>
+      </ul>
+    </div>
+  );
+}
